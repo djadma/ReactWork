ef2965b31a02886be3d71b9e257112605bfb048b
Dipak <jadamdipak@gmail.com>
2018-12-03 17:29:36 +0530

    Added layout

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,16 @@
+import Nav from './Nav';
+
+const Header = () => (
+  <div>
+    <div className="bar">
+       <a href="">Dipak Fits</a>
+       <Nav />
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <div>Crad</div>
+  </div>
+)
+
+export default Header;
