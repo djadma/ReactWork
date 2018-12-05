bc81684fe3d970d686190ac042be130e416e5540
Dipak <jadamdipak@gmail.com>
2018-12-05 17:03:27 +0530

    Setup graphql yoga

--- dipak-fits/backend/src/index.js
+++ dipak-fits/backend/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+
+// Todo use express middleware to handle cookies (JWT)
+// Todo use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  }, 
+  deets => {
+    console.log(`server running is ruuning on port http:/localhost:${deets.port}`) 
+  }
+);
--- dipak-fits/backend/src/resolvers/Mutation.js
+++ dipak-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info){
+    // Todo check if they logged in
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info)
+
+    console.log(item);
+    return item;
+  }
+};
+
+module.exports = Mutations;
