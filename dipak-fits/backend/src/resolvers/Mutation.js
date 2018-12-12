ee62344b0427023571dc7d509501f5b9a27e8e57
Dipak <jadamdipak@gmail.com>
2018-12-12 18:55:40 +0530

    Added user data

--- dipak-fits/backend/src/resolvers/Mutation.js
+++ dipak-fits/backend/src/resolvers/Mutation.js
@@ -10,6 +10,28 @@ const Mutations = {
 
     console.log(item);
     return item;
+  },
+  updateItem(parent, args, ctx, info){
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info)
+  },
+  async deleteItem(parent, args, ctx, info){
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // 2. check if then own that item, or have the permissions
+    // Todo
+    // 3. delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
